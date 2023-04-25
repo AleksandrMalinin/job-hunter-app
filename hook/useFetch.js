3b91be31c587f1a625cc87912480cf60e1f225c3
AleksandrMalinin <alexander.s.malinin@gmail.com>
2023-04-25 15:28:05 +0100

    Update useFetch hook

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,5 +1,8 @@
 import { useState, useEffect } from "react"
 import axios from "axios"
+import { RAPID_API_KEY } from "@env"
+
+const rapidApiKey = RAPID_API_KEY
 
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([])
@@ -10,7 +13,7 @@ const useFetch = (endpoint, query) => {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "c4910db27amsh731257bb83b0374p148845jsn39cd9839fe38",
+      "X-RapidAPI-Key": rapidApiKey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com"
     },
     params: { ...query }
